06ec75a613af01e338d1b89af6d8f4c269de122c
Jeff Korcal <jkorcal@gmail.com>
2016-12-01 00:52:12 -0800

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+  html: path.join(__dirname, 'client/index.html'),
+  src: path.join(__dirname, 'client/src'),
+  dist: path.join(__dirname, '/dist')
+}
+
+module.exports = {
+  entry: {
+    javascript: PATHS.src,
+    html: PATHS.html
+  },
+  output: {
+    path: PATHS.dist,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.less$/,
+        loader : 'style!css!less'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: [ 'babel-loader' ],
+        query: {
+          presets: [ 'react', 'es2015' ]
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  }
+}
